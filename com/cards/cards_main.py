1b0bf2bf30b90a140979833323c81bbfec3370c7
cnn <729481944@qq.com>
2018-12-29 20:16:34 +0800

    create card

--- com/cards/cards_main.py
+++ com/cards/cards_main.py
@@ -1,2 +1,37 @@
 #--*--coding:utf-8
 #Author:cnn
+import sys
+from com.cards.cards_tools import *
+#欢迎
+print("名片管理系统".center(46,"*"))
+str_1="1、新建名片"
+str_2="2、操作名片"
+str_3="3、查询名片"
+print(str_1)
+print(str_2)
+print(str_3)
+print("*"*50)
+
+
+#1,2,3针对名片的操作
+#0退出系统
+#输入其它,提示输入错误
+    # 输入操作
+action_str = input("请选择您希望执行的操作:")
+print("您选择的操作是【%s】" % action_str)
+action = int(action_str)
+if action==0:
+    print("退出系统")
+    sys.exit(1)
+elif action==1:
+    #print(str_1)
+    create_card()
+    write_file()
+elif action==2:
+    print(str_2)
+elif action==3:
+    print(str_3)
+else:
+    print("输入错误")
+
+
--- com/cards/cards_tools.py
+++ com/cards/cards_tools.py
@@ -1,2 +1,22 @@
 #--*--coding:utf-8
 #Author:cnn
+card_dict={}
+card_list=[]
+def create_card():
+    card_name=input("请输入姓名:")
+    card_qq=input("请输入qq:")
+    card_phone=input("请输入电话:")
+    card_email=input("请输入邮箱:")
+    card_dict["name"]=card_name
+    card_dict["qq"]=card_qq
+    card_dict["phone"]=card_phone
+    card_dict["email"]=card_email
+    card_list.append(card_dict)
+def write_file():
+    with open("D:/python/script/CardManage/files/file.txt","a") as f:
+        f.write(str(card_dict)+"\n")
+# if __name__=="__main__":
+#     create_card()
+#     print(card_dict)
+#     print(card_list)
+#     write_file()
